016e6361faeea47f6da748a05adc155eca056682
Starli0n <Starli0n@users.noreply.github.com>
2016-07-07 20:19:10 +0200

    First launch

---
+++ config/user-startup.cmd
@@ -0,0 +1,6 @@
+:: use this file to run your own startup commands
+:: use @ in front of the command to prevent printing the command
+
+:: @call "D:\Tools\PortableGit/cmd/start-ssh-agent.cmd
+:: @set PATH=%CMDER_ROOT%\vendor\whatever;%PATH%
+
